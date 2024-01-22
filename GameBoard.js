b3cd690d26095fd147350fb765b8ba86432c590e
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-22 18:52:07 +0100

    working on grids

---
+++ GameBoard.js
@@ -0,0 +1,36 @@
+import { GRID_SIZE, OBJECT_TYPE, CLASS_LIST, LEVEL } from "./setup";
+
+class GameBoard {
+  constructor(DOMGrid) {
+    this.dotCount = 0;
+    this.grid = [];
+    this.DOMGrid = DOMGrid;
+  }
+
+  //   showGameStatus(gameWin) {}
+
+  createGrid(level) {
+    this.dotCount = 0;
+    this.grid = [];
+    this.DOMGrid.innerHTML = "";
+    // First set correct amount of columns based on Grid Size and Cell Size
+    this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px);`;
+
+    level.forEach((square) => {
+      const div = document.createElement("div");
+      div.classList.add("square", CLASS_LIST[square]);
+      div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+      this.DOMGrid.appendChild(div);
+      this.grid.push(div);
+
+      // Add dots
+      if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
+    });
+  }
+
+  static createGameBoard(DOMGrid, level) {
+    const board = new this(DOMGrid);
+    board.createGrid(level);
+    return board;
+  }
+}
---
+++ setup.js
@@ -0,0 +1,44 @@
+export const GRID_SIZE = 20;
+export const CELL_SIZE = 20;
+
+export const OBJECT_TYPE = {
+    BLANK: 'blank',
+    WALL: 'wall',
+    DOT: 'dot',
+    GHOST: 'ghost',
+    PACMAN: 'pacman',
+    SCARED: 'scared',
+    GHOSTLAIR: 'lair',
+};
+
+export const CLASS_LIST = [
+    OBJECT_TYPE.BLANK,
+    OBJECT_TYPE.WALL,
+    OBJECT_TYPE.DOT,
+]
+
+export const LEVEL = [
+    1,1,1,1,1,
+    1,2,2,2,1,
+    1,1,2,1,1,
+    1,2,2,2,1,
+    1,1,1,1,1,
+]
+
+// export const DIRECTIONS = {
+//     ArrowLeft: {
+//         movement: -1,
+//         rotation: 180,
+//     },
+//     ArrowUp: {
+//         movement: -GRID_SIZE,
+//         rotation: 270,
+//     },
+//     ArrowRight: {
+//         movement: 1,
+//         rotation: 0,
+//     }, //     ArrowDown: { //         movement: GRID_SIZE,
+//         rotation: 90,
+//     },
+// };
+
