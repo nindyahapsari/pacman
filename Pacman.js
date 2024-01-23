8181e2dcdc87efa8060b646f3ed6b06b5d6fbfdf
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-23 16:52:31 +0100

    class: create pacman class basic function, render it to the board to test

---
+++ Pacman.js
@@ -0,0 +1,65 @@
+import { GRID_SIZE, OBJECT_TYPE, CLASS_LIST, DIRECTIONS } from "./setup.js";
+
+class Pacman {
+  constructor(speed, startPosition) {
+    this.position = startPosition;
+    this.speed = speed;
+    this.direction = null;
+    this.timer = 0;
+    this.powerPill = false;
+    this.rotation = true;
+  }
+
+  shouldMove() {
+    if (!this.direction) return;
+
+    if (this.timer === this.speed) {
+      this.timer = 0;
+      return true;
+    }
+
+    this.timer++;
+  }
+
+  getNextMove(objectExist) {
+    //check wall and ghost lair
+
+    let nextMovePosition = this.position + this.direction.movement;
+
+    if (
+      objectExist(nextMovePosition, OBJECT_TYPE.WALL) ||
+      objectExist(nextMovePosition, OBJECT_TYPE.GHOSTLAIR)
+    ) {
+      nextMovePosition = this.position;
+    }
+
+    return { nextMovePosition, direction: this.direction };
+  }
+
+  makeMovementInBoard() {
+    const classToRemove = [OBJECT_TYPE.PACMAN];
+    const classToAdd = [OBJECT_TYPE.PACMAN];
+
+    return { classToRemove, classToAdd };
+  }
+
+  setNewPosition(nextMovePosition) {
+    this.position = nextMovePosition;
+  }
+
+  handleKeyInput = (e, objectExist) => {
+    let directions;
+
+    if (e.keycode >= 37 && e.keycode <= 40) {
+      directions = DIRECTIONS[e.key];
+    } else {
+      return;
+    }
+
+    const nextMovePosition = this.position + directions.movement;
+    if (objectExist(nextMovePosition, OBJECT_TYPE.WALL)) return;
+    this.direction = directions;
+  };
+}
+
+export default Pacman;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { LEVEL } from "./setup.js";
+import { LEVEL, OBJECT_TYPE } from "./setup.js";
 
 import GameBoard from "./GameBoard.js";
+import Pacman from "./Pacman.js";
 
 const gameGrid = document.querySelector("#game");
 const scoreDom = document.querySelector("#score");
@@ -35,6 +36,8 @@ function startGame() {
   gameBoard.createGrid(LEVEL);
 
   // init pacman stuff
+  const pacman = new Pacman(2, 287);
+  gameBoard.addObject(287, [OBJECT_TYPE.PACMAN]);
 
   // array of ghosts
 }
