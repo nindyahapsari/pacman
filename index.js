1c6585ef2b7f99868643ab92ff5693548d846f7d
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-23 11:47:37 +0100

    add key movement, fix index variable name, add base functions

--- index.js
+++ index.js
@@ -3,8 +3,40 @@ import { LEVEL } from "./setup.js";
 import GameBoard from "./GameBoard.js";
 
 const gameGrid = document.querySelector("#game");
+const scoreDom = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
 
 const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
-gameBoard.createGrid(LEVEL);
 
-console.log(LEVEL);
+// Initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function playAudio() {}
+
+function gameOver() {}
+
+function checkCollision() {}
+
+function gameLoop() {}
+
+function startGame() {
+  console.log("start game");
+
+  gameWin = false;
+  powerPillActive = false;
+  score = 0;
+
+  startButton.classList.add("hide");
+
+  gameBoard.createGrid(LEVEL);
+
+  // init pacman stuff
+
+  // array of ghosts
+}
+
+startButton.addEventListener("click", startGame);
